dcdf192d8c1cfba4af6b5f3d5e11438045312195
Shreyans Jain <shreyansj551@gmail.com>
2020-02-05 23:20:35 +0530

    remove hardcoded contacts

--- scripts/ui.js
+++ scripts/ui.js
@@ -23,4 +23,104 @@ navtext.forEach((item) => {
     item.style.color = "rgb(223, 223, 223)";
   }
 })
-/* ---------------------------- */
+
+
+/* Render comtacts */
+const contactData = {
+  dvm: {
+    name: "Manish Thakur",
+    post: "Website and Registrations",
+    phone: "+91-9876543210",
+    email: "webmaster@bits-apogee.org",
+    image: "assets/contact/Business_Man-512.png"
+  },
+  pcr: {
+    name: "Manish Thakur",
+    post: "Website and Registrations",
+    phone: "+91-9876543210",
+    email: "webmaster@bits-apogee.org",
+    image: "assets/contact/Business_Man-512.png"
+  },
+  pep: {
+    name: "Manish Thakur",
+    post: "Website and Registrations",
+    phone: "+91-9876543210",
+    email: "webmaster@bits-apogee.org",
+    image: "assets/contact/Business_Man-512.png"
+  },
+  spons: {
+    name: "Manish Thakur",
+    post: "Website and Registrations",
+    phone: "+91-9876543210",
+    email: "webmaster@bits-apogee.org",
+    image: "assets/contact/Business_Man-512.png"
+  },
+  recnacc: {
+    name: "Manish Thakur",
+    post: "Website and Registrations",
+    phone: "+91-9876543210",
+    email: "webmaster@bits-apogee.org",
+    image: "assets/contact/Business_Man-512.png"
+  },
+  adp: {
+    name: "Manish Thakur",
+    post: "Website and Registrations",
+    phone: "+91-9876543210",
+    email: "webmaster@bits-apogee.org",
+    image: "assets/contact/Business_Man-512.png"
+  },
+  controls: {
+    name: "Manish Thakur",
+    post: "Website and Registrations",
+    phone: "+91-9876543210",
+    email: "webmaster@bits-apogee.org",
+    image: "assets/contact/Business_Man-512.png"
+  },
+  pres: {
+    name: "Manish Thakur",
+    post: "Website and Registrations",
+    phone: "+91-9876543210",
+    email: "webmaster@bits-apogee.org",
+    image: "assets/contact/Business_Man-512.png"
+  },
+  gensec: {
+    name: "Manish Thakur",
+    post: "Website and Registrations",
+    phone: "+91-9876543210",
+    email: "webmaster@bits-apogee.org",
+    image: "assets/contact/Business_Man-512.png"
+  }
+}
+
+let contactBody = document.querySelector(".contact-body");
+for (let costaan in contactData) {
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let img = document.createElement("img");
+  img.src = contactData[costaan].image;
+
+  let name = document.createElement("div");
+  name.classList.add("name");
+  name.innerHTML = contactData[costaan].name;
+
+  let department = document.createElement("div");
+  department.classList.add("department");
+  department.innerHTML = contactData[costaan].post;
+
+  let phone = document.createElement("div");
+  phone.classList.add("phone");
+  phone.innerHTML = contactData[costaan].phone;
+
+  let email = document.createElement("div");
+  email.classList.add("email");
+  email.innerHTML = contactData[costaan].email;
+
+  card.appendChild(img);
+  card.appendChild(name);
+  card.appendChild(department);
+  card.appendChild(phone);
+  card.appendChild(email);
+  contactBody.appendChild(card);
+}
+
