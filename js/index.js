f2804287213e7bfde1df057e83dada72f0163eb5
mukul0596 <icecoolmkl@gmail.com>
2020-02-16 15:06:09 +0530

    added complete loader anim

--- js/index.js
+++ js/index.js
@@ -188,5 +188,7 @@ const navigateTo = (id) => {
 window.onload = () => {
     setTime();
     document.getElementsByClassName('grid-background')[0].style.height = document.body.scrollHeight - document.getElementById('home').clientHeight + 'px';
-    document.getElementById('loader').style.display = 'none';
+    setTimeout(() => {
+      document.getElementById('loader').style.display = 'none';
+    }, 9000)
 }
