c5ec2375f647716eaf9fd1e5425ce5c65d5f3bba
Manan Agrawal <44023521+mananag@users.noreply.github.com>
2021-03-11 14:43:09 +0530

    workshop dropdown created

--- js/register.js
+++ js/register.js
@@ -1,7 +1,10 @@
 const REGISTRATIONS_URL = 'https://bits-apogee.org/registrations/Register/';
 const COLLEGE_URL = 'https://bits-apogee.org/registrations/get_college';
 const EVENT_URL = 'https://bits-apogee.org/registrations/events';
+const WORKSHOP_URL = 'https://bits-apogee.org/registrations/workshops';
 
+
+//EVENTS
 const SELECTED_EVENTS = [];
 const noEventMessage = document.getElementById('no-event-message');
 const selectedEventsContainer = document.getElementsByClassName('selected-events-container')[0];
@@ -20,6 +23,29 @@ document.getElementById("events").addEventListener("keydown", function(e) {
     e.preventDefault();
 });
 
+
+
+
+//WORKSHOPS
+const SELECTED_WORKSHOPS = [];
+const noWorkshopMessage = document.getElementById('no-workshop-message');
+const selectedWorkshopsContainer = document.getElementsByClassName('selected-workshops-container')[0];
+
+console.log(document.getElementById("workshop"));
+document.getElementById("workshop").addEventListener("keypress", function(e) {
+    console.log("yeah");
+    e.preventDefault();
+});
+
+document.getElementById("workshop").addEventListener("keyup", function(e) {
+    e.preventDefault();
+});
+
+document.getElementById("workshop").addEventListener("keydown", function(e) {
+    e.preventDefault();
+});
+
+
 const getCollegesList = () => {
     fetch(COLLEGE_URL).then(data => {
         return data.json();
@@ -36,13 +62,15 @@ const getCollegesList = () => {
     }, console.error);
 }
 
+
+//EVENTS
 const getEventsList = () => {
     fetch(EVENT_URL).then(data => {
         return data.json()
     }).then(response => {
         document.getElementsByTagName('datalist')[1].removeChild(document.getElementById('loadingEvent'));
         response.data.map(category => {
-            // if (category.category_name === 'Movie (Test)')
+            if (category.category_name != 'Workshop') {
                 category.events.map(event => {
                     const option = document.createElement("option");
                     option.value = event.name;
@@ -50,9 +78,12 @@ const getEventsList = () => {
                     option.innerHTML = event.name;
                     document.getElementById('event_input').appendChild(option);
                 })
+            }
         })
-    }, console.error) 
+    }, console.error)
 }
+
+
 
 const handleEventClick = (e) => {
     console.log("cross trigerred");
@@ -107,6 +138,88 @@ const handleEventClick = (e) => {
 }
 
 document.querySelector('input[list="event_input"]').addEventListener('input', handleEventClick)
+
+
+
+
+
+
+//WORKSHOPS
+const getWorkshopsList = () => {
+    fetch(EVENT_URL).then(data => {
+        return data.json()
+    }).then(response => {
+        // document.getElementsByTagName('datalist')[1].removeChild(document.getElementById('loadingWorkshop'));
+        response.data.map(category => {
+            if (category.category_name === 'Workshop') {
+                console.log(category)
+                category.events.map(event => {
+                    // console.log(event)
+                    const option = document.createElement("option");
+                    option.value = event.name;
+                    option.id = event.id;
+                    option.innerHTML = event.name;
+                    document.getElementById('workshop_input').appendChild(option);
+                })
+            }
+        })
+    }, console.error)
+}
+
+const handleWorkshopClick = (e) => {
+    console.log("cross trigerred");
+    const input = e.target;
+    const workshopName = input.value;
+    e.target.value = "";
+    // e.target.blur();
+    const list = input.getAttribute('list');
+    const options = document.getElementById(list).childNodes;
+
+    const workshopTag = document.createElement("div");
+    workshopTag.classList.add("selected-workshop");
+    workshopTag.innerHTML = workshopName;
+
+    const removeButton = document.createElement("img");
+    removeButton.src = "assets/icons/red-close.png";
+    removeButton.alt = "Remove";
+
+    for(var i = 0; i < options.length; i++) {
+        if(options[i].innerText === workshopName) {
+            SELECTED_WORKSHOPS.push(parseInt(options[i].id));
+            options[i].disabled = true;
+            const that = options[i];
+            removeButton.onclick = (e) => {
+                e.target.parentNode.remove();
+                console.log(e.target.parentNode);
+                for (let i = 0; i < SELECTED_WORKSHOPS.length; i++) {
+                    if (SELECTED_WORKSHOPS[i] == that.id) {
+                        SELECTED_WORKSHOPS.splice(i, 1);
+                    }
+                    if (SELECTED_WORKSHOPS.length === 0) {
+                        noWorkshopMessage.style.display = 'initial';
+                        selectedWorkshopsContainer.style.border = '3px solid #0fffff';
+                    }
+                }
+                that.disabled = false;
+            }
+        }
+    }
+
+    workshopTag.appendChild(removeButton);
+    document.getElementsByClassName("selected-workshops-container")[0].appendChild(workshopTag);
+
+    if (SELECTED_WORKSHOPS.length === 0) {
+        noWorkshopMessage.style.display = 'initial';
+        selectedWorkshopsContainer.style.border = '3px solid #0fffff';
+    }
+    if (SELECTED_WORKSHOPS.length !== 0) {
+        noWorkshopMessage.style.display = 'none';
+        selectedWorkshopsContainer.style.border = '3px solid #b02c8d';
+    }
+}
+
+document.querySelector('input[list="workshop_input"]').addEventListener('input', handleWorkshopClick)
+
 
 const getCollegeId = () => {
     const input = document.getElementById('college');
@@ -214,4 +327,5 @@ function showData(id) {
 window.onload = () => {
     getCollegesList();
     getEventsList();
+    getWorkshopsList();
 }
