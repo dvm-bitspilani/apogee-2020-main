797cef8379bc244655c443112f0c8ff9351d3a76
igarg2001 <igarg2001@gmail.com>
2021-03-21 17:40:27 +0530

    register dialog

--- js/kindstore.js
+++ js/kindstore.js
@@ -62,8 +62,16 @@ const registerForKindstore = () => {
   const body2 = {
     name: name,
     email: email,
-   phone: phone,
+    phone: phone,
   };
+  events_dialog(
+    {
+      desc: "<p>Registering you... Please Wait</p>",
+    },
+    false,
+    true,
+    true
+  );
   fetch("https://bits-apogee.org/kindstore/register/", {
     method: "POST",
     headers: {
@@ -80,9 +88,115 @@ const registerForKindstore = () => {
     })
     .then((data) => {
       console.log(data);
+      close_events();
+      setTimeout(() => {
+        events_dialog(
+          {
+            desc: `<p>${data.message}</p>`,
+          },
+          false,
+          true,
+          true
+        );
+        setTimeout(() => {
+          togglePage("form-section", "store-section", true);
+        }, 300);
+      }, 300);
     })
     .catch((err) => {
       console.log(err);
-      Promise.resolve(err).then(data => console.log(data));
+      Promise.resolve(err).then((data) => console.log(data));
     });
+};
+
+function events_dialog(input, comingFromKindstore, register, hideCloseButton) {
+  console.log(comingFromKindstore);
+  //console.log(input.desc.length)
+  //console.log(document.getElementsByClassName("events-dialogue-container"));
+  // document.getElementsByClassName("events-dialogue-container")[0].style.top =
+  //   window.pageYOffset + "px";
+  document.getElementsByClassName(
+    "events-dialogue-container"
+  )[0].style.display = "flex";
+  setTimeout(() => {
+    document.getElementsByClassName(
+      "events-dialogue-container"
+    )[0].style.opacity = "1";
+  }, 100);
+
+  setTimeout(() => {
+    document.getElementsByClassName("events-dialogue-box")[0].style.transform =
+      "translateY(0vh)";
+  }, 500);
+  document.getElementsByClassName("events-dialogue-text")[0].innerHTML =
+    input.desc;
+  if (comingFromKindstore) {
+    // document
+    //   .getElementsByClassName("box-redeem-button")[1]
+    //   .setAttribute("onClick", `cancelDetails(${input.desc});`);
+    document.getElementsByClassName("box-redeem-button")[1].onclick = () =>
+      cancelDetails(input.desc);
+    document.getElementsByClassName("first-step")[0].style.display = "block";
+    document.getElementsByClassName("second-step")[0].style.display = "none";
+  }
+  if (register) {
+    document.getElementsByClassName("first-step")[0].style.display = "none";
+    document.getElementsByClassName("second-step")[0].style.display = "none";
+    if (hideCloseButton)
+      document.getElementsByClassName(
+        "close_button_container"
+      )[0].style.display = "none";
+  }
+  //   setTimeout(() => {
+  //     document.getElementsByClassName("events-dialogue-text")[0].style.transform =
+  //       "translateY(0vh)";
+  //   }, 100);
+  // document.getElementsByClassName('events-dialogue-text')[0].style.transform = 'translateY(0vh)'
+  //disableScroll();
+}
+function close_events(comingFromKindstore) {
+  document.getElementsByClassName(
+    "events-dialogue-container"
+  )[0].style.opacity = "0";
+
+  setTimeout(() => {
+    document.getElementsByClassName("events-dialogue-box")[0].style.transform =
+      "translateY(-100vh)";
+    document.getElementsByClassName(
+      "events-dialogue-container"
+    )[0].style.display = "none";
+  }, 200);
+  if (comingFromKindstore) {
+    const firstStep = document.querySelector(".first-step");
+    const secondStep = document.querySelector(".second-step");
+    firstStep.style.display = "block";
+    secondStep.style.display = "none";
+  }
+  //   document.getElementsByClassName("events-dialogue-text")[0].style.transform =
+  //     "translateY(-100vh)";
+
+  //enableScroll();
+}
+
+// function disableScroll() {
+//   // Get the current page scroll position
+//   scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//   (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
+//     // if any scroll is attempted, set this to the previous value
+//     (window.onscroll = function () {
+//       window.scrollTo(scrollLeft, scrollTop);
+//     });
+// }
+
+// function enableScroll() {
+//   window.onscroll = function () {};
+// }
+
+const cancelDetails = (desc) => {
+  const eventsDialogText = document.querySelector(".events-dialogue-text");
+  const firstStep = document.querySelector(".first-step");
+  const secondStep = document.querySelector(".second-step");
+  eventsDialogText.innerHTML = desc;
+  firstStep.style.display = "block";
+  secondStep.style.display = "none";
 };
